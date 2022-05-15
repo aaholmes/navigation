89d2fd867f1e20e12aaa42b18977a14ae0c887c0
Adam Holmes <aah95@cornell.edu>
2022-05-15 00:53:28 -0400

    Move plots to results dir

--- results/plot.py
+++ results/plot.py
@@ -2,13 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 for i in range(5):
-    df1 = pd.read_csv('results/dqn/' + str(i) + '.txt')
+    df1 = pd.read_csv('dqn/' + str(i) + '.txt')
     df1["avg"] = df1.rolling(100, min_periods=1).mean()
     plt.plot(df1["avg"], 'g')
-    df1 = pd.read_csv('results/ddqn/' + str(i) + '.txt')
+    df1 = pd.read_csv('ddqn/' + str(i) + '.txt')
     df1["avg"] = df1.rolling(100, min_periods=1).mean()
     plt.plot(df1["avg"], 'r')
-    df1 = pd.read_csv('results/ddqn_rand/' + str(i) + '.txt')
+    df1 = pd.read_csv('ddqn_rand/' + str(i) + '.txt')
     df1["avg"] = df1.rolling(100, min_periods=1).mean()
     plt.plot(df1["avg"], 'b')
 plt.title("Episodes to solve Bananas environment")
